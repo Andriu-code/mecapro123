14536fb3b9e65b8fe18ab3e59216bbd3878daaaf
Andriu_code <a.lealhernandez91@gmail.com>
2026-05-21 08:28:00 -0600

    Update ProductCarousel.tsx

--- src/components/carousel/ProductCarousel.tsx
+++ src/components/carousel/ProductCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./ProductCarousel.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
